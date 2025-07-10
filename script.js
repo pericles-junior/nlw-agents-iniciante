b5c1ceda657452b661cdd614b8a4201a7afb86a4
Péricles Júnior <periclesmjr@hotmail.com>
2025-07-09 23:15:09 -0300

    Retirando API Key

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ const markdownToHTML = (text) => {
   return converter.makeHtml(text)
 }
 
-// AIzaSyCivE62InDh85xbZCxwLvBOfao0B0USBiw
 const perguntarAI = async (question, game, apiKey) => {
   const model = "gemini-2.5-flash"
   const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
